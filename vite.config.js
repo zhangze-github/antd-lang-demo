42bd9e5666f877875fc38cf8bc8d3fc126b7e1ec
zhangze-github <1183460943@qq.com>
2022-10-24 08:43:36 +0000

    init

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginImp from 'vite-plugin-imp';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),
     vitePluginImp({
